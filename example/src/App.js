a8c59404cbac43a4ea85c8267807c61277731e88
musicq <liang.kui@hotmail.com>
2018-12-21 18:41:54 +0800

    feat: imporve example case

--- example/src/App.js
+++ example/src/App.js
@@ -1,31 +1,60 @@
 import React, { Component } from 'react';
-import { of } from 'rxjs';
+import { BehaviorSubject, combineLatest, of } from 'rxjs';
+import { map, tap } from 'rxjs/operators';
 import { VirtualList } from 'vist';
 
+const data = new Array(5000).fill(0).map((_, i) => i);
+
 class App extends Component {
+  state = {
+    data: new BehaviorSubject([]),
+    keyWord: ''
+  };
+  search$ = new BehaviorSubject(this.state.keyWord);
+  data$ = of(data);
+
   constructor(props) {
     super(props);
 
-    const data = new Array(5000).fill(0).map((_, i) => i);
+    this.onSearch = this.onSearch.bind(this);
+  }
 
-    this.state = {
-      data: of(data)
-    };
+  componentDidMount() {
+    combineLatest(this.data$, this.search$).pipe(
+      tap(([data, keyWord]) => this.setState({ keyWord })),
+      map(([data, keyWord]) => data.filter(item => item.toString().includes(keyWord)))
+    ).subscribe(data => this.state.data.next(data));
+  }
+
+  onSearch(e) {
+    this.search$.next(e.target.value);
   }
 
   render() {
     return (
-      <VirtualList
-        data$={this.state.data}
-        options$={of({ height: 60 })}
-        style={{ height: 400, border: '1px solid black' }}
-      >
-        {item => (
-          <p style={{ height: 59, margin: 0, borderBottom: '1px solid green' }}>
-            No. {item}
-          </p>
-        )}
-      </VirtualList>
+      <div>
+        <div className="input-box">
+          <label>
+            Place enter the number (0 - 5000)
+            <br/>
+            <input placeholder="Place enter the number here" autoFocus type="text" onChange={this.onSearch} value={this.state.keyWord}/>
+          </label>
+        </div>
+
+        <div className="virtual-box">
+          <VirtualList
+            data$={this.state.data}
+            options$={of({ height: 60 })}
+            style={{ height: 400 }}
+          >
+            {item => (
+              <p style={{ height: 59, margin: 0, borderBottom: '1px solid green' }}>
+                No. {item}
+              </p>
+            )}
+          </VirtualList>
+        </div>
+      </div>
     );
   }
 }
